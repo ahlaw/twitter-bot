50528c9b023308984e5143c0e3e9a1ff9831d8cd
Amos <amos.law98@gmail.com>
2017-01-22 00:10:06 -0500

    Update

---
+++ credentials.py
@@ -0,0 +1,7 @@
+keys = {
+'user_id': '817896817560055811',
+'consumer_key': '0asmAWycsjZn8hsj5kOxD3EVu',
+'consumer_secret': 'EGkRwnDawoEiZfKNXOah2qaPN3OwP89PL0Odp9Svzxd1RolzEc',
+'access_token': '817896817560055811-BalubfctT0BvOcH49YkmSpbBssEwdB9',
+'access_secret': 'dWFtFFMluhcNCtpKbpOQvLryxTAWXlwquliffo0umv7R0',
+}
